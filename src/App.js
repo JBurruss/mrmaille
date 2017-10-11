d697e37af9bb8725d17a3cb9839cb6e16c0c25e1
James <burrussjames@gmail.com>
2017-10-11 17:34:17 -0400

    added login button framework

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Login from './components/Login/Login';
+import './App.css';
+
+const App = () => 
+  <div>
+    <Login />    
+  </div>;
+
+export default App;
---
+++ src/components/Login/Login.js
@@ -0,0 +1,9 @@
+import React from "react";
+import './Login.css';
+
+const Login = () =>
+<div>
+<button id="login">Login</button>
+</div>
+
+export default Login;
