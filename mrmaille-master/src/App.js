0dd21d4e45a1b3287792d61d38b486896177d0e7
James <burrussjames@gmail.com>
2017-10-16 20:10:10 -0400

    Add files via upload

---
+++ mrmaille-master/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Login from './components/Login/Login';
+import './App.css';
+import Nav from './components/Nav/Nav';
+
+const App = () => 
+  <div>
+    <Login /> 
+    <Nav />   
+  </div>;
+
+export default App;
