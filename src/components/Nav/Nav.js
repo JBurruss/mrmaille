88fff421e830cfd2047885e3a35388055e34cf7a
James <burrussjames@gmail.com>
2017-10-16 20:43:47 -0400

    Add files via upload

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,15 +1,23 @@
 import React from "react";
 import './Nav.css';
 
-const Nav = () =>
-<div>
-<ul>
+class Nav extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <ul>
   <li><a href="#home">Home</a></li>
   <li><a href="#search">Search</a></li>
   <li><a href="#contact">Contact</a></li>
   <li id = "Title">Mr Maille</li>
-  <li><a href="#Login" class = "active">Login</a></li>  
+  <li><a href="#Login" className="active">Login</a></li>  
  </ul>
-</div>
+      </div>
+    );
+  }
+}
 
 export default Nav;
