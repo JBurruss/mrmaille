a2187f66f9a82e89758e94bd06d7794feb059a11
James <burrussjames@gmail.com>
2017-10-28 11:44:29 -0400

    fixed navbar issue floating over item field

---
+++ src/data/products.js
@@ -0,0 +1,31 @@
+const data = [
+    {
+      id: 1,
+      name: 'Hauberk',
+      price: 89.99,
+      stockQuantity: 25,
+      image: 'img/Hauberk.jpg',
+    },
+    {
+      id: 2,
+      name: 'Hauberaon',
+      price: 55.99,
+      stockQuantity: 25,
+      image: 'img/02.jpg',
+    },
+    {
+      id: 4,
+      name: 'Coif',
+      price: 29.99,
+      stockQuantity: 25,
+      image: 'img/04.jpg',
+    },
+    {
+      id: 6,
+      name: 'Pixane',
+      price: 35.99,
+      stockQuantity: 25,
+      image: 'img/06.jpg',
+    }
+]
+  export default data;
